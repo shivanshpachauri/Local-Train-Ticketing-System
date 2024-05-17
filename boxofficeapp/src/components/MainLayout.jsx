15113e8b794303c3512c8ad153480299ec3fc6fa
shivanshpachauri <shivanshpachauri00@gmail.com>
2024-05-17 09:13:19 +0530

    deploying the app

---
+++ boxofficeapp/src/components/MainLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Navs from "./Navs";
+import AppTitle from "./AppTitle";
+const MainLayout=()=>{
+    return (
+    <div>
+        <Navs />
+        <AppTitle />
+        <Outlet/>
+        </div>);
+};
+export default MainLayout;
